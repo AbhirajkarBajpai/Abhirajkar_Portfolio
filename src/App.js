6220569b428ece4969dbf67f239c04953e966087
Abhirajkar Bajpai <21IT3001@rgipt.ac.in>
2024-09-23 20:24:54 +0530

    Homepage Building

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/header/Navbar";
-import Profile_Pic from './assets/img/Ab.jpg';
+import Profile_Pic from "./assets/img/Ab.jpg";
 
 function App() {
   return (
@@ -8,12 +8,23 @@ function App() {
       <div className="home">
         <Navbar />
         <div className="profile-section">
-          <img src={Profile_Pic} alt="Profile" className="profile-pic" />
-          <span className="Headling_about">Building Websites, DAPPS , learning Fullstack, Blockchain, Trying Photography </span>
+          <div className="PN_cont">
+            <img src={Profile_Pic} alt="Profile" className="profile-pic" />
+            <div className="name_cont">
+              <span className="name_header">Abhirajkar Bajpai</span>
+            </div>
+          </div>
+          <span className="Headling_about">
+            Building Websites, DAPPS , learning Fullstack, Blockchain, Trying
+            Photography{" "}
+          </span>
           <button
             className="project-button"
             onClick={() =>
-              window.open("https://github.com/AbhirajkarBajpai?tab=repositories", "_blank")
+              window.open(
+                "https://github.com/AbhirajkarBajpai?tab=repositories",
+                "_blank"
+              )
             }
           >
             Latest Project
