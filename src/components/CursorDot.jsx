27524418d077e90e3c6308b71dbcd71c737e490f
Abhirajkar Bajpai <21IT3001@rgipt.ac.in>
2024-09-24 02:43:23 +0530

    Homepage Building

--- src/components/CursorDot.jsx
+++ src/components/CursorDot.jsx
@@ -9,7 +9,6 @@ const CursorDot = () => {
     const handleMouseMove = (e) => {
       const { clientX, clientY } = e;
       setPosition({ x: clientX, y: clientY });
-      console.log({ x: clientX, y: clientY });
       if (clientY <= 0 || clientY > window.innerHeight || clientX <= 0 || clientX > window.innerWidth) {
         setIsVisible(false);
       } else {
