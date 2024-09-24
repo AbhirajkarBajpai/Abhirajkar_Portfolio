155982ba3a3058b761eac839e0f354bd9740dc0a
Abhirajkar Bajpai <21IT3001@rgipt.ac.in>
2024-09-24 15:17:15 +0530

    Parallax effect

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -35,24 +35,24 @@ function SkillsSection() {
   return (
       <div id="skills" className="skills-section" ref={sectionRef}>
       <div className="header">
-        <span role="img" aria-label="skills-icon" className="emoji">💼</span>
-        <h1>Skills</h1>
+        <span role="img" aria-label="skills-icon" className="emoji_main">💼</span>
+        <h1>My Awesome Skills</h1>
       </div>
       <div className="skills-container">
+      <div className="skill-card">
+          <span role="img" aria-label="web-development" className="emoji">💻</span>
+          <h2>Web Development</h2>
+          <p>Building interactive and responsive websites. Utilise framwork such as React, Next for Frontend and Node , Express for Backend</p>
+        </div>
         <div className="skill-card">
           <span role="img" aria-label="photography" className="emoji">📸</span>
           <h2>Photography</h2>
-          <p>Capture moments with precision and creativity.</p>
-        </div>
-        <div className="skill-card">
-          <span role="img" aria-label="web-development" className="emoji">💻</span>
-          <h2>Web Development</h2>
-          <p>Building interactive and responsive websites.</p>
+          <p>Capture moments with precision and creativity. trying some cinematic shots, color Grading and after Effect.</p>
         </div>
         <div className="skill-card">
           <span role="img" aria-label="blockchain" className="emoji">🔗</span>
           <h2>Blockchain</h2>
-          <p>Exploring decentralized solutions with blockchain technology.</p>
+          <p>Exploring decentralized solutions with blockchain technology. Developed Notable Dapps using Ethereum Network, worked with Solidity, ether.js and Tools like Ganache</p>
         </div>
       </div>
     </div>
